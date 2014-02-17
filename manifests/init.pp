ea39673aa000e73ed034b92aef4990f2eebf9e90
Wayne Ingram <w.ingram@nib.com.au>
2014-02-17 12:05:54 +1100

    Inital Commit

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+################################################################################
+#
+# Class to install Repositories on RPM distros
+#
+# Author: Wayne Ingram
+#
+class rpm_repos {
+  case $::operatingsystem {
+    'RedHat', 'CentOS': {
+      yumrepo {
+        'nib':
+          baseurl => 'http://styx/CentOS/6/local/x86_64',
+          enabled => 1,
+          name => 'ingram',
+          gpgcheck => 0;
+      }
+      default: {
+        fail("Unsupport OS $::operatingsystem");
+      }
+    }
+  }
+}
